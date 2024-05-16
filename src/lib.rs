2439ac7d9f33abb72fd4ded67a6e19e82927e15d
Lokathor <zefria@gmail.com>
2024-05-15 23:30:41 -0600

    fake_blx

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,17 @@
 extern crate proc_macro;
+use core::sync::atomic::{AtomicU64, Ordering};
+
 use proc_macro::{Literal, TokenStream, TokenTree};
 
 const SPSR_ALLOWED_TARGET_REG_LIST: &[&str] = &[
   "r0", "R0", "r1", "R1", "r2", "R2", "r3", "R3", "r4", "R4", "r5", "R5", "r6",
   "R6", "r7", "R7", "r8", "R8", "r9", "R9", "r10", "R10", "r11", "R11", "r12",
   "R12", "r14", "R14", "lr", "LR",
+];
+const ANY_REG_NAME: &[&str] = &[
+  "r0", "R0", "r1", "R1", "r2", "R2", "r3", "R3", "r4", "R4", "r5", "R5", "r6",
+  "R6", "r7", "R7", "r8", "R8", "r9", "R9", "r10", "R10", "r11", "R11", "r12",
+  "R12", "r13", "R13", "r14", "R14", "r15", "R15", "lr", "LR", "pc", "PC",
 ];
 
 fn get_str_literal(tree: &TokenTree) -> Option<String> {
@@ -19,6 +26,14 @@ fn get_str_literal(tree: &TokenTree) -> Option<String> {
       }
     }
   }
+}
+
+fn next_u64() -> u64 {
+  static NEXT: AtomicU64 = AtomicU64::new(0);
+  NEXT.fetch_add(1, Ordering::Relaxed)
+}
+fn next_local_label() -> String {
+  format!(".L_bracer_local_label_{}", next_u64())
 }
 
 /// Reads SPSR to a named register.
@@ -98,7 +113,42 @@ pub fn write_spsr(token_stream: TokenStream) -> TokenStream {
   if !SPSR_ALLOWED_TARGET_REG_LIST.contains(&reg_name.as_str()) {
     panic!("register name `{reg_name}` is not on the permitted list")
   }
-  let output = format!("mrs {reg_name}, SPSR");
+  let output = format!("msr {reg_name}, SPSR");
+
+  TokenStream::from(TokenTree::Literal(Literal::string(&output)))
+}
+
+/// Fakes a `blx` type of operation.
+///
+/// Emits a string literal of `a32` code like the following:
+/// ```arm
+/// adr lr .L_bracer_local_label_<id>
+/// bx <reg>
+/// .L_bracer_local_label_<id>:
+/// ```
+///
+/// Every expansion uses a uniquely generated local label id, so this shouldn't
+/// ever clash with any other code.
+///
+/// This can only be used in `a32` state, because you can't move values directly
+/// to `lr` in `t32` state.
+#[proc_macro]
+pub fn a32_fake_blx(token_stream: TokenStream) -> TokenStream {
+  let trees: Vec<TokenTree> = token_stream.into_iter().collect();
+  let tree = match &trees[..] {
+    [tree] => tree,
+    _ => panic!("please provide only a single ident or string literal"),
+  };
+  let reg_name = get_str_literal(tree).expect("input must be a string literal");
+  if !ANY_REG_NAME.contains(&reg_name.as_str()) {
+    panic!("register name `{reg_name}` is not on the permitted list")
+  }
+  let label = next_local_label();
+  let output = format!(
+    "adr lr, {label}
+    bx {reg_name}
+    {label}:"
+  );
 
   TokenStream::from(TokenTree::Literal(Literal::string(&output)))
 }
--- tests/the_tests.rs
+++ tests/the_tests.rs
@@ -1,4 +1,4 @@
-use bracer::{read_spsr, write_spsr};
+use bracer::{a32_fake_blx, read_spsr, write_spsr};
 
 #[test]
 fn test_read_spsr() {
@@ -12,4 +12,25 @@ fn test_write_spsr() {
   assert_eq!(write_spsr!("r0"), "msr r0, SPSR");
   assert_eq!(write_spsr!("R0"), "msr R0, SPSR");
   assert_eq!(write_spsr!("lr"), "msr lr, SPSR");
+}
+
+#[test]
+fn test_a32_fake_blx() {
+  let asm_lines = a32_fake_blx!("r0");
+  let mut lines = asm_lines.lines();
+  let mut line = lines.next().unwrap();
+  let mut line_iter = line.split(' ');
+
+  assert_eq!(line_iter.next().unwrap(), "adr");
+  assert_eq!(line_iter.next().unwrap(), "lr,");
+  let local_label = line_iter.next().unwrap();
+  assert!(line_iter.next().is_none());
+
+  line = lines.next().unwrap();
+  assert_eq!(line.trim(), "bx r0");
+
+  line = lines.next().unwrap();
+  assert_eq!(line.trim(), format!("{local_label}:"));
+
+  assert!(lines.next().is_none());
 }
