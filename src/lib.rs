7668f53fd0b4c84ec56918ad0ae76d866aa71f97
Lokathor <zefria@gmail.com>
2024-05-15 22:50:05 -0600

    writing

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,26 @@
 extern crate proc_macro;
 use proc_macro::{Literal, TokenStream, TokenTree};
 
+const SPSR_ALLOWED_TARGET_REG_LIST: &[&str] = &[
+  "r0", "R0", "r1", "R1", "r2", "R2", "r3", "R3", "r4", "R4", "r5", "R5", "r6",
+  "R6", "r7", "R7", "r8", "R8", "r9", "R9", "r10", "R10", "r11", "R11", "r12",
+  "R12", "r14", "R14", "lr", "LR",
+];
+
+fn get_str_literal(tree: &TokenTree) -> Option<String> {
+  match tree {
+    TokenTree::Group(_) | TokenTree::Ident(_) | TokenTree::Punct(_) => None,
+    TokenTree::Literal(l) => {
+      let string = format!("{l}");
+      if string.starts_with('"') && string.ends_with('"') {
+        Some(string[..string.len() - 1][1..].to_string())
+      } else {
+        None
+      }
+    }
+  }
+}
+
 /// Reads SPSR to a named register.
 ///
 /// This evaluates to a string literal for the following assembly:
@@ -33,28 +53,52 @@ pub fn read_spsr(token_stream: TokenStream) -> TokenStream {
     [tree] => tree,
     _ => panic!("please provide only a single ident or string literal"),
   };
-  const ALLOWED_TARGET_REG_LIST: &[&str] = &[
-    "r0", "R0", "r1", "R1", "r2", "R2", "r3", "R3", "r4", "R4", "r5", "R5",
-    "r6", "R6", "r7", "R7", "r8", "R8", "r9", "R9", "r10", "R10", "r11", "R11",
-    "r12", "R12", "r14", "R14", "lr", "LR",
-  ];
-  match tree {
-    TokenTree::Group(_) => panic!("expected str literal, found group"),
-    TokenTree::Punct(_) => {
-      panic!("expected ident or str literal, found punctuation")
-    }
-    TokenTree::Ident(_) => {
-      panic!("expected ident or str literal, found identifier")
-    }
-    TokenTree::Literal(l) => {
-      let string = format!("{l}");
-      let without_quotes = &string[..string.len() - 1][1..];
-      if !ALLOWED_TARGET_REG_LIST.contains(&without_quotes) {
-        panic!("register name `{without_quotes}` is not on the permitted list")
-      }
-      let output = format!("mrs {without_quotes}, SPSR");
-
-      TokenStream::from(TokenTree::Literal(Literal::string(&output)))
-    }
+  let reg_name = get_str_literal(tree).expect("input must be a string literal");
+  if !SPSR_ALLOWED_TARGET_REG_LIST.contains(&reg_name.as_str()) {
+    panic!("register name `{reg_name}` is not on the permitted list")
   }
+  let output = format!("mrs {reg_name}, SPSR");
+
+  TokenStream::from(TokenTree::Literal(Literal::string(&output)))
+}
+
+/// Writes SPSR from a named register.
+///
+/// This evaluates to a string literal for the following assembly:
+///
+/// ```arm
+/// msr <reg>, SPSR   // 'move special <-- register'
+/// ```
+///
+/// Input should be a string literal naming the register that holds the value
+/// you want to write to SPSR.
+///
+/// ## Panics
+/// * If the target register isn't allowed to be used with the `msr`
+///   instruction, this will panic. You can use `r0` through `r12`, and `lr`
+///   (aka `r14`). You can also use any of those registers written with
+///   uppercase.
+///
+/// ## Assembly Safety
+/// * From the [ARM Docs]: *You must not attempt to access the SPSR when the
+///   processor is in User or System mode. This is your responsibility. The
+///   assembler cannot warn you about this, because it has no information about
+///   the processor mode at execution time.*
+///
+/// [ARM Docs]:
+///     https://developer.arm.com/documentation/dui0489/i/arm-and-thumb-instructions/mrs--psr-to-general-purpose-register-
+#[proc_macro]
+pub fn write_spsr(token_stream: TokenStream) -> TokenStream {
+  let trees: Vec<TokenTree> = token_stream.into_iter().collect();
+  let tree = match &trees[..] {
+    [tree] => tree,
+    _ => panic!("please provide only a single ident or string literal"),
+  };
+  let reg_name = get_str_literal(tree).expect("input must be a string literal");
+  if !SPSR_ALLOWED_TARGET_REG_LIST.contains(&reg_name.as_str()) {
+    panic!("register name `{reg_name}` is not on the permitted list")
+  }
+  let output = format!("mrs {reg_name}, SPSR");
+
+  TokenStream::from(TokenTree::Literal(Literal::string(&output)))
 }
--- tests/the_tests.rs
+++ tests/the_tests.rs
@@ -1,8 +1,15 @@
-use bracer::read_spsr;
+use bracer::{read_spsr, write_spsr};
 
 #[test]
 fn test_read_spsr() {
   assert_eq!(read_spsr!("r0"), "mrs r0, SPSR");
   assert_eq!(read_spsr!("R0"), "mrs R0, SPSR");
   assert_eq!(read_spsr!("lr"), "mrs lr, SPSR");
+}
+
+#[test]
+fn test_write_spsr() {
+  assert_eq!(write_spsr!("r0"), "msr r0, SPSR");
+  assert_eq!(write_spsr!("R0"), "msr R0, SPSR");
+  assert_eq!(write_spsr!("lr"), "msr lr, SPSR");
 }
